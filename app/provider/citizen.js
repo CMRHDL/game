cd96087f686a8619886b7c4fb717e5fb3eb18b6a
CMR <cmrhdl1337@gmail.com>
2015-10-10 10:12:14 +0200

    added maxage, age by season

--- app/provider/citizen.js
+++ app/provider/citizen.js
@@ -8,13 +8,26 @@
 
     var service = this; // jshint ignore:line
 
-    service.create = function (id, age, education, religion, happyness) {
-      return new Citizen(id, age, education, religion, happyness);
+    service.create = function(id, age, seas, education, religion, happyness) {
+      return new Citizen(id, age, seas, education, religion, happyness);
     };
 
-    function Citizen(id, age, education, religion, happyness) {
+    service.advanceTime = function(citArr, season) {
+      var tempArr = [];
+      for(var i = 0, len = citArr.length; i < len; i++) {
+        citArr[i].age = citArr[i].season === season ? citArr[i].age + 1 : citArr[i].age;
+        if(citArr[i].age < 51) {
+          tempArr.push(citArr[i]);
+        }
+      }
+      citArr = tempArr;
+      console.log(citArr.length);
+    };
+
+    function Citizen(id, age, season, education, religion, happyness) {
       this.id = id;
       this.age = age;
+      this.season = season;
       this.education = education;
       this.religion = religion;
       this.happyness = happyness;
--- app/views/x/all.controller.js
+++ app/views/x/all.controller.js
@@ -8,10 +8,12 @@
 
     all.citizenCount = 1;
     all.citizens = [];
+    all.maxExpectedAge = 50;
 
     all.advanceTime = advanceTime;
     all.createNewCitizens = createNewCitizens;
     all.getAvgAge = getAvgAge;
+    all.getMaxExpectedAge = getMaxExpectedAge;
     all.getCount = getCount;
     all.getDate = getDate;
 
@@ -21,6 +23,7 @@
 
     var educations = ['none', 'basic', 'high'];
     var religions = ['reformed', 'christian', 'protestant'];
+    var seasons = ['Winter', 'Spring', 'Summer', 'Autumn'];
 
     init();
     function init(){
@@ -33,23 +36,22 @@
       }
       season = getNextSeason(season);
       getDate();
+      citizen.advanceTime(all.citizens, season);
     }
 
     function getNextSeason(currentSeas) {
-      var nextSeas = {
-        'Winter' : 'Spring',
-        'Spring' : 'Summer',
-        'Summer' : 'Autumn',
-        'Autumn' : 'Winter',
+      var nextSeas = {};
+      for(var i = 0, len = seasons.length; i < len; i++) {
+        nextSeas[seasons[i]] = seasons[i+1] ? seasons[i+1] : seasons[0];
       }
       return nextSeas[currentSeas];
     }
 
     function createNewCitizens(count) {
       for(var i = 1; i <= count; i++) {
-        // all.citizens.push({id: all.citizenCount, age: getAge(), education: getEducation(), religion: getReligion(), happyness: getHappyness()});
-        // all.citizens.push(new Citizen(all.citizenCount, getAge(), getEducation(), getReligion(), getHappyness()));
-        all.citizens.push(citizen.create(all.citizenCount, getAge(), getEducation(), getReligion(), getHappyness()));
+        // all.citizens.push({id: all.citizenCount, age: getAge(), education: getEducation(), season: getSeason(), religion: getReligion(), happyness: getHappyness()});
+        // all.citizens.push(new Citizen(all.citizenCount, getAge(), getSeason() getEducation(), getReligion(), getHappyness()));
+        all.citizens.push(citizen.create(all.citizenCount, getAge(), getSeason(), getEducation(), getReligion(), getHappyness()));
         all.citizenCount++;
       }
       console.log(all.citizens);
@@ -73,6 +75,11 @@
     function getAge() {
       roll = randomNumber(50);
       return roll;
+    }    
+
+    function getSeason() {
+      roll = randomNumber(seasons.length);
+      return seasons[roll-1];
     }
 
     function getReligion() {
@@ -101,8 +108,17 @@
       return Math.floor(totalAge / all.citizenCount);
     }
 
+    function getMaxExpectedAge() {
+      return all.maxExpectedAge;
+    }
+
     function randomNumber(to) {
       return Math.floor((Math.random() * to) + 1);
+    }
+
+    function setCitizen(cit) {
+      // [{id: 1, age: 20, education: 'high', season: 'Winter', religion: 'protestant', happyness: 5}]
+      all.citizens = cit;
     }
 
     all.reliData = chart.getDataArray(getCount('religion'));
